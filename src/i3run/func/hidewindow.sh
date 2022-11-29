8a3bbbf2c272fe76b55b8b5ab6d6062d68dd5fb5
valar <68338647+76616c6172@users.noreply.github.com>
2022-11-29 12:16:55 -0800

    change(i3run): move app to at least workspace 5+ when sending it "away"
    I like having workspaces 1-4 available for applications I don't have
    bound to a dedicated key, so it's annoying when say vscode or my
    main terminal with tmux panes get sent to workspaces 1-4.
    This change the behavior of hidewindow.sh so every time a
    workspace is sent "away" it will be sent to the next free workspace
    UNLESS the next free workspace is < 5.

--- src/i3run/func/hidewindow.sh
+++ src/i3run/func/hidewindow.sh
@@ -6,6 +6,12 @@ hidewindow() {
       # send window to the next free workspace (not the scratchpad!)
       o_ws=$(i3-msg -t get_workspaces  | jq '.[] | .num' | tail -n 1)
       n_ws=`expr $o_ws + 1`
+      if [[ $n_ws = "1" ]] || [[ $n_ws = "2" ]] || [[ $n_ws = "3" ]] || [[ $n_ws = "4" ]]; then
+        n_ws=9
+      fi
+      if [[ $n_ws = "9" ]] ; then
+        n_ws=`expr $n_ws + 1`
+      fi
       messy "[con_id=${i3list[TWC]}]" move to workspace $n_ws
       i3var set "hidden${i3list[TWC]}" "${i3list[TWF]}"
     else
